229ef2a82534fee24e6c241c676383b422851316
nbaerair <nicolaus.baer@airbus.com>
2024-12-11 07:35:26 -0500

    add GET_TIME_2

--- a653_inc/a653Time.h
+++ a653_inc/a653Time.h
@@ -15,11 +15,14 @@
 
 /* function declarations */
 
-extern void TIMED_WAIT (SYSTEM_TIME_TYPE DELAY_TIME, 
+extern void TIMED_WAIT (SYSTEM_TIME_TYPE DELAY_TIME, /* 64bit - 1 nanosecond LSB */
 			RETURN_CODE_TYPE * RETURN_CODE);
+
 extern void PERIODIC_WAIT (RETURN_CODE_TYPE * RETURN_CODE);
-extern void GET_TIME (SYSTEM_TIME_TYPE * SYSTEM_TIME,
+
+extern void GET_TIME (SYSTEM_TIME_TYPE * SYSTEM_TIME, /* 64bit - 1 nanosecond LSB */
 		      RETURN_CODE_TYPE * RETURN_CODE);
+
 extern void REPLENISH (SYSTEM_TIME_TYPE   BUDGET_TIME, 
                        RETURN_CODE_TYPE * RETURN_CODE);
 
