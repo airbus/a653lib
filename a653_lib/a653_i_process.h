1714fcc7d374ca934b590731617a23c78f5ee334
Patrick Siegl <3261314+psiegl@users.noreply.github.com>
2025-06-06 20:46:52 +0200

    Expose struct prcs_info_t for later

--- a653_lib/a653_i_process.h
+++ a653_lib/a653_i_process.h
@@ -40,6 +40,21 @@
 typedef void *(*__start_routine) (void *);
 typedef void (*func_ptr)(void);
 
+typedef struct {
+  //  PROCESS_ATTRIBUTE_TYPE attr;
+ 
+  pthread_mutex_t        t_lock;
+  pthread_t              t_ctx;
+  pthread_attr_t         t_attr;
+  int64_t                timerPeriod;
+  struct timespec        nextActivation;
+  unsigned int           priority;
+  unsigned int           cycle_cnt;
+  unsigned short         id;
+  char                   name[35];
+  func_ptr               prcs_main_func;
+} prcs_info_t;
+
 
 void a653_act_prcs(void);
 
