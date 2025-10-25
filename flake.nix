0e9bdacd3d481ba504d2d84894ae21a612dd3131
Patrick Siegl <3261314+psiegl@users.noreply.github.com>
2025-10-25 22:04:33 +0200

    NixOS attempt

--- flake.nix
+++ flake.nix
@@ -5,33 +5,53 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" "powerpc64-linux"] (system:
+    flake-utils.lib.eachSystem ["x86_64-linux" "aarch64-linux" "powerpc64-linux"] (system:
       let
         # import the nixpkgs, passing the current system as arg
         pkgs = import nixpkgs {
           inherit system;
         };
+
+        # Add needed build tools via overrideAttrs (do NOT pass nativeBuildInputs to callPackage)
+        addBuildDeps = pkg: pkg.overrideAttrs (oldAttrs: {
+          nativeBuildInputs = (oldAttrs.nativeBuildInputs or []) ++ [ pkgs.git pkgs.gcc pkgs.gnu_make ];
+          buildInputs = (oldAttrs.buildInputs or []) ++ [ pkgs.git ];
+        });
       in
       {
         packages = rec {
-          # define default package
-          default = a653lib;
-
           # just call the package with the normal stdenv
           a653lib = pkgs.callPackage pkgs/a653lib.nix { };
+
+          # Apply overrideAttrs here, not an argument to callPackage
+          a653libWithDeps = addBuildDeps a653lib;
+
+          default = a653libWithDeps;
 
           # override the stdenv to use an older gcc
           a653lib-gcc11 =
             let
               stdenv = pkgs.overrideCC pkgs.stdenv pkgs.gcc11;
+              base = pkgs.callPackage pkgs/a653lib.nix { inherit stdenv; };
             in
-            pkgs.callPackage pkgs/a653lib.nix { inherit stdenv; };
+            base.overrideAttrs (oldAttrs: {
+              nativeBuildInputs = (oldAttrs.nativeBuildInputs or []) ++ [ pkgs.git pkgs.gcc pkgs.gnumake ];
+              buildInputs = (oldAttrs.buildInputs or []) ++ [ pkgs.git ];
+            });
 
           # override the build to be with debug symbols (source still missing though)
-          a653lib-debug = pkgs.enableDebugging a653lib;
+          a653lib-debug = pkgs.enableDebugging a653libWithDeps;
 
           # cross compile for a pre-defined target platform
-          a653lib-aarch64 = pkgs.pkgsCross.aarch64-multiplatform-musl.pkgsStatic.callPackage pkgs/a653lib.nix { };
+          a653lib-aarch64 =
+            let
+              pkgsCross = pkgs.pkgsCross.aarch64-multiplatform-musl.pkgsStatic;
+              base = pkgsCross.callPackage pkgs/a653lib.nix { };
+            in
+            base.overrideAttrs (oldAttrs: {
+              nativeBuildInputs = (oldAttrs.nativeBuildInputs or []) ++ [ pkgsCross.git pkgsCross.gcc pkgsCross.gnumake ];
+              buildInputs = (oldAttrs.buildInputs or []) ++ [ pkgsCross.git ];
+            });
 
          # cross compile for a custom defined target platform
           a653lib-armv7a =
@@ -41,8 +61,12 @@
                 inherit system;
                 crossSystem.config = "armv7l-unknown-linux-musleabi";
               };
+              base = pkgsCross.callPackage pkgs/a653lib.nix { };
             in
-            pkgsCross.callPackage pkgs/a653lib.nix { };
+            base.overrideAttrs (oldAttrs: {
+              nativeBuildInputs = (oldAttrs.nativeBuildInputs or []) ++ [ pkgsCross.git pkgsCross.gcc pkgsCross.gnumake ];
+              buildInputs = (oldAttrs.buildInputs or []) ++ [ pkgsCross.git ];
+            });
 
           # cross compile for a custom defined target platform
           a653lib-powerpc64 =
@@ -52,8 +76,12 @@
                 inherit system;
                 crossSystem.config = "powerpc64-unknown-linux-gnuabielfv2";
               };
+              base = pkgsCross.pkgsStatic.callPackage pkgs/a653lib.nix { };
             in
-            pkgsCross.pkgsStatic.callPackage pkgs/a653lib.nix { };
+            base.overrideAttrs (oldAttrs: {
+              nativeBuildInputs = (oldAttrs.nativeBuildInputs or []) ++ [ pkgsCross.git pkgsCross.gcc pkgsCross.gnumake ];
+              buildInputs = (oldAttrs.buildInputs or []) ++ [ pkgsCross.git ];
+            });
         };
       }
     );
