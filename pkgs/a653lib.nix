892b4daa9743459964bb44c59d83cd89778a8433
Patrick Siegl <3261314+psiegl@users.noreply.github.com>
2025-11-03 21:54:03 +0100

    Added Nix

--- pkgs/a653lib.nix
+++ pkgs/a653lib.nix
@@ -1,4 +1,12 @@
-{ lib, stdenv, fetchFromGitHub, bintools }:
+{ lib, stdenv, fetchurl, fetchFromGitHub, bintools }:
+
+let
+  arinc653Zip = fetchurl {
+    url = "https://brx-content.fullsight.org/site/binaries/content/assets/itc/content/support-files/arinc653.h.zip";
+    sha256 = "1xml8ki31pidxp5gzyqb4bp819809zpx4kkg2k5x1nhar50gxjp2";
+    curlOptsList = [ "--user-agent" "Mozilla/5.0 (Windows NT 6.3; WOW64; rv:41.0) Gecko/20100101 Firefox/41.0" ];
+  };
+in
 
 stdenv.mkDerivation {
   pname = "a653lib";
@@ -23,6 +31,8 @@ stdenv.mkDerivation {
   installPhase = ''
     mkdir --parent -- $out/bin
     mv $HOME/bin/* $out/bin
+
+    cp ${arinc653Zip}/ARINC653.h $(HOME)/tmp/download/ARINC653.h
   '';
   dontStrip = true;
 
